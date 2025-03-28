04c6cb3f10b257f7001f9db7760b10e25874ef18
Илья <ila@MacBook-Air-Mac.IGD_Rostelecom>
2025-03-29 01:19:00 +0300

    настроено подключение к БД

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,3 +5,15 @@ const userController = require('../src/controllers/userController');
 router.get('/', userController.getUsers);
 
 module.exports = router;
+
+
+const pool = require('./db');
+
+// Пример использования
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Ошибка выполнения запроса', err);
+    return;
+  }
+  console.log('Результат запроса:', res.rows);
+});
---
+++ src/db.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Ошибка подключения к базе данных', err);
+  } else {
+    console.log('Успешное подключение к базе данных');
+    release(); // Освобождаем клиента после использования
+  }
+});
+
+module.exports = pool;
