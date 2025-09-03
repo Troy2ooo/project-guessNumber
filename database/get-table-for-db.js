613d0be7f453fa0f9c3795422543fcb250ac4bd0
Илья <ila@macbook-air-mac.home>
2025-09-03 17:04:28 +0200

    fix failed merge

--- database/get-table-for-db.js
+++ database/get-table-for-db.js
@@ -22,6 +22,27 @@ async function createTables() {
           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
           updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );
+
+
+        CREATE TABLE IF NOT EXISTS authors (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(255) NOT NULL,
+          bio TEXT,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+
+
+        CREATE TABLE IF NOT EXISTS books_authors (
+          book_id INT NOT NULL,
+          author_id INT NOT NULL,
+          PRIMARY KEY (book_id, author_id),
+          FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE,
+          FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE
+        );
+
+
+
       `);
     console.log("Таблицы созданы");
   } catch (error) {
